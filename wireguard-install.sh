33747d12ca6243c32e763076c7d680fab32ade0d
Yablowsky <42838586+Yablowsky@users.noreply.github.com>
2026-04-08 03:49:24 +0000

    Revert "Update wireguard-install.sh"
    This reverts commit a5689513a2111335f7709e7d1178c545fad6a466.

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -102,13 +102,13 @@ function getHomeDirForClient() {
 		# if not, use SUDO_USER
 		if [ "${SUDO_USER}" == "root" ]; then
 			# If running sudo as root
-			HOME_DIR="/root/opt/wg/clients"
+			HOME_DIR="/root"
 		else
 			HOME_DIR="/home/${SUDO_USER}"
 		fi
 	else
 		# if not SUDO_USER, use /root
-		HOME_DIR="/root/opt/wg/clients"
+		HOME_DIR="/root"
 	fi
 
 	echo "$HOME_DIR"
